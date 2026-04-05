3c0376be2be17b05eaa12cdc292707567184ec0b
Jaans Pravin Nadar <factomedia15@gmail.com>
2026-04-05 16:29:39 +0530

    Day 1 : int, double, float , char , boolean , String

---
+++ 01-basics/DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int age= 19; // for integar values
+        double price =90.9; // decimals
+        float amount = 12.0f; 
+        char grade = 'A'; // mostly used in string and array problems
+        boolean con = true; // conditions
+        String name ="Jaans"; // mostly used in the development 
+
+        System.out.println("int: "+ age);
+        System.out.println("double: "+price);
+        System.out.println("float: "+amount);
+        System.out.println("char: "+ grade);
+        System.out.println("boolean value: "+con);
+        System.out.println("String: "+ name);
+         
+    }
+}
