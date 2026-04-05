56b26904b9b16e79181c028b205c1396c0eb9681
Jaans Pravin Nadar <factomedia15@gmail.com>
2026-04-05 15:37:59 +0530

    Day 1 : HelloWorld - first Java Program

---
+++ 01-basics/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        //So this is my First program 
+        System.out.println("Hello World");
+        System.out.println("My Name is Jaans Pravin Nadar");
+        System.out.println("So today I have Created my GitHub");
+    }
+}
