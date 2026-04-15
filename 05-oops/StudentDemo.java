c77eab5bcd18e3069ec09febb3be2d3e7c9994df
Jaans Pravin Nadar <factomedia15@gmail.com>
2026-04-15 23:08:12 +0530

    Day 10: OOP - First Class and Objects (Student)

---
+++ 05-oops/StudentDemo.java
@@ -0,0 +1,62 @@
+/*
+ * Problem: Introduction to Classes and Objects
+ *
+ * Description:
+ * Create a Student class to store and display student information.
+ * Learn how to create objects and use instance variables and methods.
+ *
+ * Expected Output:
+ * --- Student Details ---
+ * Name: Jaans Pravin
+ * Roll No: 50
+ * CGPA: 8.5
+ *
+ * Name: Sung Jinwoo
+ * Roll No: 51
+ * CGPA: 8.49
+ *
+ * KEY CONCEPTS:
+ * - Class = Blueprint/Template for creating objects
+ * - Object = Instance created from class using 'new' keyword
+ * - Instance Variables = Properties each object has (name, rollNo, cgpa)
+ * - Methods = Actions/behaviors that objects can perform
+ * - this keyword will be learned later - not needed yet!
+ *
+ * ANALOGY:
+ * Class = Cookie cutter (shape/template)
+ * Object = Actual cookie made from that cutter
+ * Each cookie can have different data (chocolate chips, plain, etc.)
+ *
+ * Concepts used: Classes, Objects, Instance Variables, Methods
+ */
+class Student{
+    String name ;
+    int rollNo;
+    double cgpa;
+
+    public void displayinfo(){              //don't use static here it will give error since it's a non static variable
+        System.out.println("Name : "+name);
+        System.out.println("Roll No : "+rollNo);
+        System.out.println("CGPA : "+cgpa);
+        System.out.println();
+
+    }
+}
+public class StudentDemo{
+    public static void main(String[] args) {
+        System.out.println("--- Student Details ---");
+        Student s1 = new Student();
+        s1.name="Jaans Pravin";
+        s1.rollNo=50;
+        s1.cgpa=8.5;
+
+        Student s2 = new Student();
+        s2.name="Sung Jinwoo";
+        s2.rollNo=51;
+        s2.cgpa=8.49;
+
+        s1.displayinfo();
+        s2.displayinfo();
+
+    }
+}
